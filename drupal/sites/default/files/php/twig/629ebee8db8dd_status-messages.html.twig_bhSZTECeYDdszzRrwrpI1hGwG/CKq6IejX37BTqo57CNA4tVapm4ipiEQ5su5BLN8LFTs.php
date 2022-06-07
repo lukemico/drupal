faa41998baf6a2c1e6a538e008af293d0e49688c
Luke Mico <lmico@sydneyoperahouse.com>
2022-06-07 13:19:07 +1000

    first commit

---
+++ drupal/sites/default/files/php/twig/629ebee8db8dd_status-messages.html.twig_bhSZTECeYDdszzRrwrpI1hGwG/CKq6IejX37BTqo57CNA4tVapm4ipiEQ5su5BLN8LFTs.php
@@ -0,0 +1,176 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/profiles/demo_umami/themes/umami/templates/components/messages/status-messages.html.twig */
+class __TwigTemplate_ba56ff78d14b937096feb95e4b2fcef3 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'messages' => [$this, 'block_messages'],
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 22
+        echo "<div data-drupal-messages>
+";
+        // line 23
+        $this->displayBlock('messages', $context, $blocks);
+        // line 57
+        echo "</div>
+";
+    }
+
+    // line 23
+    public function block_messages($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 24
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["message_list"] ?? null));
+        foreach ($context['_seq'] as $context["type"] => $context["messages"]) {
+            // line 25
+            echo "  ";
+            // line 26
+            $context["classes"] = [0 => "messages", 1 => ("messages--" . $this->sandbox->ensureToStringAllowed(            // line 28
+$context["type"], 28, $this->source))];
+            // line 31
+            echo "  <div role=\"contentinfo\" aria-label=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed((($__internal_compile_0 = ($context["status_headings"] ?? null)) && is_array($__internal_compile_0) || $__internal_compile_0 instanceof ArrayAccess ? ($__internal_compile_0[$context["type"]] ?? null) : null), 31, $this->source), "html", null, true);
+            echo "\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->withoutFilter($this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 31), 31, $this->source), "role", "aria-label"), "html", null, true);
+            echo ">
+    <div class=\"messages__content container\">
+      ";
+            // line 33
+            if (($context["type"] == "error")) {
+                // line 34
+                echo "        <div role=\"alert\">
+      ";
+            }
+            // line 36
+            echo "        ";
+            if ((($__internal_compile_1 = ($context["status_headings"] ?? null)) && is_array($__internal_compile_1) || $__internal_compile_1 instanceof ArrayAccess ? ($__internal_compile_1[$context["type"]] ?? null) : null)) {
+                // line 37
+                echo "          <h2 class=\"visually-hidden\">";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed((($__internal_compile_2 = ($context["status_headings"] ?? null)) && is_array($__internal_compile_2) || $__internal_compile_2 instanceof ArrayAccess ? ($__internal_compile_2[$context["type"]] ?? null) : null), 37, $this->source), "html", null, true);
+                echo "</h2>
+        ";
+            }
+            // line 39
+            echo "        ";
+            if ((twig_length_filter($this->env, $context["messages"]) > 1)) {
+                // line 40
+                echo "          <ul class=\"messages__list\">
+            ";
+                // line 41
+                $context['_parent'] = $context;
+                $context['_seq'] = twig_ensure_traversable($context["messages"]);
+                foreach ($context['_seq'] as $context["_key"] => $context["message"]) {
+                    // line 42
+                    echo "              <li class=\"messages__item\">";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($context["message"], 42, $this->source), "html", null, true);
+                    echo "</li>
+            ";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['message'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 44
+                echo "          </ul>
+        ";
+            } else {
+                // line 46
+                echo "          <span class=\"messages__item\">";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, twig_first($this->env, $this->sandbox->ensureToStringAllowed($context["messages"], 46, $this->source)), "html", null, true);
+                echo "</span>
+        ";
+            }
+            // line 48
+            echo "      ";
+            if (($context["type"] == "error")) {
+                // line 49
+                echo "        </div>
+      ";
+            }
+            // line 51
+            echo "    </div>
+  </div>
+  ";
+            // line 54
+            echo "  ";
+            $context["attributes"] = twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "removeClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 54);
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['type'], $context['messages'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "core/profiles/demo_umami/themes/umami/templates/components/messages/status-messages.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  126 => 54,  122 => 51,  118 => 49,  115 => 48,  109 => 46,  105 => 44,  96 => 42,  92 => 41,  89 => 40,  86 => 39,  80 => 37,  77 => 36,  73 => 34,  71 => 33,  63 => 31,  61 => 28,  60 => 26,  58 => 25,  54 => 24,  50 => 23,  45 => 57,  43 => 23,  40 => 22,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/profiles/demo_umami/themes/umami/templates/components/messages/status-messages.html.twig", "/Users/lukem/Documents/drupal/drupal/core/profiles/demo_umami/themes/umami/templates/components/messages/status-messages.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("block" => 23, "for" => 24, "set" => 26, "if" => 33);
+        static $filters = array("escape" => 31, "without" => 31, "length" => 39, "first" => 46);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['block', 'for', 'set', 'if'],
+                ['escape', 'without', 'length', 'first'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
